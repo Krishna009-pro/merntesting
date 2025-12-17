7cd5728f8372205cace7e57e98b95f36d8d04460
Krishna009-pro <kkp1882006@gmail.com>
2025-12-17 12:26:53 +0530

    Add frontend and backend code

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+ const register = async () => {
+  try {
+    const res = await axios.post("http://localhost:5000/register", {
+      username,
+      password,
+    });
+    alert(res.data.message);
+  } catch (err) {
+    alert("Registration failed");
+  }
+};
+
+
+  const login = async () => {
+  try {
+    const res = await axios.post("http://localhost:5000/login", {
+      username,
+      password,
+    });
+    alert(res.data.message);
+  } catch (err) {
+    alert(err.response?.data?.error || "Login failed");
+  }
+};
+
+
+  return (
+    <div style={{ padding: "50px" }}>
+      <h2>Register / Login</h2>
+      <input placeholder="Username" onChange={e => setUsername(e.target.value)} /><br/><br/>
+      <input placeholder="Password" type="password" onChange={e => setPassword(e.target.value)} /><br/><br/>
+      <button onClick={register}>Register</button>
+      <button onClick={login} style={{ marginLeft: "10px" }}>Login</button>
+    </div>
+  );
+}
+
+export default App;
